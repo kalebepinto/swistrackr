08cc2788ff978976d35a592efa42c33de3e3c660
Kalebe da Silva Pinto <kalebedsp@gmail.com>
2022-06-29 02:03:10 -0400

    background function update.

--- R/background.R
+++ R/background.R
@@ -2,19 +2,17 @@
 #'
 #' \code{background} is used to import an image.jpg previously created in
 #' an image editor that corresponds to a video frame used in Swistrack4
-#' that #' does not contain the tracked particle. Later, this file will
+#' that does not contain the tracked particle. Later, this file will
 #' be used to define the zones of interest to be analyzed.
 #'
-#' @param image.jpg Image in .jpg extension created in an image editor.
+#' @param image.jpg Image name in the directory, in .jpg extension created in an image editor.
 #'
-#' @return Height and Width dimensions from background video.
-#' @examples background(image.jpg = 'bg-swistrack-1.jpg')
+#' @return Height and Width dimensions from background.
+#' @examples background(image.jpg = system.file('extdata', 'bg-swistrack-1.jpg', package = 'swistrackr'))
+#'
+#' @import jpeg
 #'
 #' @export
-
-if(!require(jpeg)){
-  install.packages('jpeg')
-}
 
 background <- function(image.jpg = NA){
   if (!file.exists(image.jpg)){
@@ -23,7 +21,7 @@ background <- function(image.jpg = NA){
   if (grepl('\\.jpg$', image.jpg) == FALSE){
     stop('Not a JPEG file.', call. = F)
   }
-  img <- jpeg::readJPEG(image.jpg)
+  img <- readJPEG(image.jpg)
   dimen <- data.frame(image.jpg, dim(img)[1], dim(img)[2])
   colnames(dimen) <- c('Image Name', 'Height', 'Width')
   rownames(dimen) <- 1
